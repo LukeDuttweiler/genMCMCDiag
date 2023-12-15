f7b34c169644c72c660993112805f73cf1ca1028
LukeDuttweiler <luke.duttweiler16@houghton.edu>
2023-12-15 15:24:10 -0500

    added more documentation

--- R/transf-standardTransform.R
+++ R/transf-standardTransform.R
@@ -1,4 +1,13 @@
+#' Transforms an mcmcObj into a dataframe with no modifications to values
+#'
+#' @param mhDraws An object of class mcmcObj
+#' @param ... Not used.
+#'
+#' @return data.frame with rows that represent MCMC draws. Columns named val.1, val.2,...
+#' contain different dimensions of the draws, column named Posterior contains (non-normalized)
+#' posterior value of the draw
 standardTransform <- function(mhDraws, ...){
+
   #Will try to force into dataframe of the expected type
   mhRet <- lapply(mhDraws, function(mhObj){
     mhDF <- as.data.frame(do.call('rbind', mhObj$val))
@@ -6,6 +15,6 @@ standardTransform <- function(mhDraws, ...){
     mhDF <- cbind(mhDF, data.frame(Posterior = mhObj$Posterior, t = 1:nrow(mhDF)))
     return(mhDF)
   })
-  
+
   return(mhRet)
 }
--- R/transf-sumTransform.R
+++ R/transf-sumTransform.R
@@ -1,16 +1,23 @@
+#' Transforms MCMC draws from an mcmcObj into a data.frame using the 'sum' transformation
+#'
+#' @param mhDraws An object of class mcmcObj
+#' @param ... Catches extra arguments. Not used.
+#'
+#' @return data.frame with columns 'val.1' which is the sum of all dimensions of each MCMC
+#' draw, and 'Posterior' which is the (non-normalized) posterior value of each MCMC draw.
 sumTransform <- function(mhDraws, ...){
   #Turn each val value into sum of the parts
   mhSums <- lapply(mhDraws, function(mhList){
-    
+
     #Loop through val (as if it is a list, even if its only a vector) and calculate the distance between
     #the value and the reference for each
     sums <- sapply(mhList$val, function(v){
       return(sum(v))
     })
-    
+
     #Notice we name distance as val in the dataframe for future functions
     return(data.frame(val.1 = sums, t = 1:length(sums)))
   })
-  
+
   return(mhSums)
 }
