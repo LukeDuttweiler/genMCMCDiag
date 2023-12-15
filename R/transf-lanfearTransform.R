a9ed8dc7b63e34b0814ad727a16f2ce3aaf543a6
LukeDuttweiler <luke.duttweiler16@houghton.edu>
2023-12-15 12:19:28 -0500

    allow lanfear transform to catch unknown arguments

--- R/transf-lanfearTransform.R
+++ R/transf-lanfearTransform.R
@@ -1,9 +1,9 @@
-lanfearTransform <- function(mhDraws, distance, reference = NULL){
+lanfearTransform <- function(mhDraws, distance, reference = NULL, ...){
   #if reference isn't specified, select a random reference
   if(is.null(reference)){
     #Select a random chain
     rChain <- sample(1:length(mhDraws), 1)
-    
+
     #Select a random value from that chain
     mhChain <- mhDraws[[rChain]]
     mhChainVal <- mhChain[grepl('val', names(mhChain))]
@@ -15,22 +15,22 @@ lanfearTransform <- function(mhDraws, distance, reference = NULL){
       reference <- mhChainVal[[1]][[rNum]]
     }
   }
-  
+
   #Turn each val value into the distance from the reference
   mhDists <- lapply(mhDraws, function(mhList){
     #Extract val, ignore Posterior
     mhList <- mhList$val
-    
+
     #Loop through val (as if it is a list, even if its only a vector) and calculate the distance between
     #the value and the reference for each
     dist <- sapply(mhList, function(v){
       return(distance(v, reference))
     })
-    
+
     #Notice we name distance as val in the dataframe for future functions
     return(data.frame(val.1 = dist, t = 1:length(dist)))
   })
-  
+
   #Return transformed chains
   return(mhDists)
 }
