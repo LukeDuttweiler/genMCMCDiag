ef7bb1ed8e4afcf61a700eebfbbd0a1e89670014
LukeDuttweiler <luke.duttweiler16@houghton.edu>
2023-12-15 15:55:31 -0500

    added more documentation.

--- R/transf-lanfearTransform.R
+++ R/transf-lanfearTransform.R
@@ -1,3 +1,20 @@
+#' Transforms a list of mcmcObjs into a list of data.frames using the Lanfear transformation
+#'
+#' @param mhDraws A list of mcmcObjs
+#' @param distance Distance function defined on the space of MCMC draws. See details.
+#' @param reference Reference point (with exact same structure as each MCMC draw)
+#'  for draw comparison. If left NULL a random point is selected from the given draws.
+#'  See details.
+#' @param ... Catches extra arguments. Not used.
+#'
+#' @details
+#' The Lanfear transformation works by specifying a reference point and then comparing each
+#' MCMC draw back to that reference point using a distance function. The function returns
+#' this distance value as the Lanfear transformation of each draw.
+#'
+#' @return List of data.frames with columns 'val.1' which is the Lanfear transformation of each MCMC
+#' draw, 'Posterior' which is the (non-normalized) posterior value of each MCMC draw and 't'
+#' which gives the within-chain ordering of the MCMC draws. Each data.frame is a separate chain.
 lanfearTransform <- function(mhDraws, distance, reference = NULL, ...){
   #if reference isn't specified, select a random reference
   if(is.null(reference)){
--- R/transf-likelihoodTransform.R
+++ R/transf-likelihoodTransform.R
@@ -1,15 +1,24 @@
+#' Transforms a list of mcmcObjs into a list of data.frames using the 'likelihood'
+#' transformation
+#'
+#' @param mhDraws A list of mcmcObjs
+#' @param ... Catches extra arguments. Not used.
+#'
+#' @return List of data.frames with columns 'val.1' which is non-normalized
+#' posterior of each draw and 't' which gives the within-chain ordering of the MCMC draws.
+#'  Each data.frame is a separate chain.
 likelihoodTransform <- function(mhDraws, ...){
   #Throw error if no Posterior value
   if(is.null(mhDraws[[1]]$Posterior)){
     stop('The likelihood method requires posterior values for each chain.')
   }
-  
+
   #Extract Posteriors
   likelihood <- lapply(mhDraws, function(df){
     return(data.frame(val.1 = df$Posterior, t = 1:length(df$Posterior)))
   })
-  
-  #Notice we name likelihood as val in the dataframe. 
+
+  #Notice we name likelihood as val in the dataframe.
   #This is because standardTraceplot() will plot the variable
   #named val on the y-axis as desired
   return(likelihood)
