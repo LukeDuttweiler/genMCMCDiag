c04ad9790d4d729db627a034b90c04cf175cb5aa
LukeDuttweiler <luke.duttweiler16@houghton.edu>
2023-12-22 11:18:30 -0500

    Added possible dpmm distance function and documentation

--- R/distances.R
+++ R/distances.R
@@ -42,7 +42,8 @@ hammingDist <- function(x,y){
 
 #' Partition Distance
 #'
-#' Function to return the 'Partition' distance between two objects.
+#' Function to return the 'Partition' distance between two objects. Used for Bayesian
+#' Networks with the 'partition-MCMC' algorithm.
 #'
 #' @param x Data.frame with columns node and partition
 #' @param y Data.frame with columns node and partition. Same nrows as x.
@@ -65,4 +66,38 @@ partitionDist <- function(x, y){
 
   #Take absolute difference of partition levels
   return(sum(abs(x$partition - y$partition)))
+}
+
+#' DPMM Distance
+#'
+#' For an MCMC draw from a DPMM D_x, let Z_x be the vector of Z-scores of the
+#' observations based on that observation's current group, and let A_x be the
+#' 0,1 adjacency matrix where \deqn{[A_x]_{ij} = 1} if observations i and j are in
+#' the same group in draw D_x (so the diagonal is always 1s).
+#' Then we define the DPMM distance between D_x and D_y as: \deqn{
+#'    d(D_x, D_y) = |Z_x - Z_y|^T(|A_x - A_y| + I)|Z_x - Z_y|.
+#' }
+#'
+#' @param x List with elements 'Zscore' and 'Adj'
+#' @param y List with elements 'Zscore' and 'Adj', both of same dimensions as in x.
+#'
+#' @note
+#' For speed, no error handling if x and y do not have the same dimensions. The function
+#' will break if 'Zscore' or 'Adj' doesn't exist though.
+#'
+#' @return Numeric, DPMM distance between x and y.
+#' @export
+#'
+dpmmDistance <- function(x, y){
+  #Extract elements
+  Zx <- x$Zscore
+  Zy <- y$Zscore
+  Ax <- x$Adj
+  Ay <- y$Adj
+
+  #Make distance and return
+  Zdiff <- abs(Zx - Zy)
+  Adiff <- abs(Ax - Ay) + diag(1, nrow(A))
+
+  return(t(Zdiff) %*% Adiff %*% Zdiff)
 }
